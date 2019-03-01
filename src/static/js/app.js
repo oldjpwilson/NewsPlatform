83eee0b67c5cea2e3ce64f177f59a65ae038dc5b
mattfreire <freire1996@gmail.com>
2019-03-02 00:17:18 +0200

    New navbar and search styling

--- src/static/js/app.js
+++ src/static/js/app.js
@@ -1,96 +1,31 @@
-$(document).foundation();
+/*global $, document, Chart, LINECHART, data, options, window, setTimeout*/
+$(document).ready(function() {
+  "use strict";
 
-function search_focus() {
-    setTimeout(function () {
-        setTimeout(function x() {
-            $('#search-input').focus();
-        }, 10);
-        x();
-    }, 100);
-}
+  // ---------------------------------------------- //
+  // Preventing URL update on navigation link click
+  // ---------------------------------------------- //
+  $(".link-scroll").bind("click", function(e) {
+    var anchor = $(this);
+    $("html, body")
+      .stop()
+      .animate(
+        {
+          scrollTop: $(anchor.attr("href")).offset().top + 2
+        },
+        700
+      );
+    e.preventDefault();
+  });
 
-var codes = document.getElementsByTagName("code");
-for(var i = 0; i < codes.length; i++){
-    $(codes[i]).attr('id','id_code'+(i+1));
-}
-
-
-
-
-// using jQuery
-function getCookie(name) {
-    var cookieValue = null;
-    if (document.cookie && document.cookie !== '') {
-        var cookies = document.cookie.split(';');
-        for (var i = 0; i < cookies.length; i++) {
-            var cookie = jQuery.trim(cookies[i]);
-            // Does this cookie string begin with the name we want?
-            if (cookie.substring(0, name.length + 1) === (name + '=')) {
-                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
-                break;
-            }
-        }
-    }
-    return cookieValue;
-}
-
-var csrftoken = getCookie('csrftoken');
-
-function csrfSafeMethod(method) {
-    // these HTTP methods do not require CSRF protection
-    return (/^(GET|HEAD|OPTIONS|TRACE)$/.test(method));
-}
-
-
-$.ajaxSetup({
-    beforeSend: function(xhr, settings) {
-        if (!csrfSafeMethod(settings.type) && !this.crossDomain) {
-            xhr.setRequestHeader("X-CSRFToken", csrftoken);
-        }
-    }
+  // ---------------------------------------------- //
+  // Search Bar
+  // ---------------------------------------------- //
+  $(".search-btn").on("click", function(e) {
+    e.preventDefault();
+    $(".search-area").fadeIn();
+  });
+  $(".search-area .close-btn").on("click", function() {
+    $(".search-area").fadeOut();
+  });
 });
-
-$(document).ready(function(){
-    var $myForm = $('#subscription_ajax')
-    $myForm.submit(function(event){
-        event.preventDefault()
-        var $formData = $(this).serialize()
-        $.ajax({
-            method: "POST",
-            url: '/subscribe/',
-            data: $formData,
-            success: handleFormSuccess,
-            error: handleFormError,
-        })
-    })
-
-    function handleFormSuccess(response){
-        // console.log(data)
-        if(response.status == 404) {
-            iziToast.error({
-                title: 'Email already register',
-                message: '',
-            });
-
-        }
-
-        else{
-            iziToast.success({
-                title: 'Thank you',
-                message: 'Successfully subscribed you to mailing list',
-            });
-        }
-
-    }
-
-    function handleFormError(jqXHR, textStatus, errorThrown){
-        console.log(jqXHR)
-        console.log(textStatus)
-        console.log(errorThrown)
-
-        iziToast.error({
-                title: 'Error occured !',
-                message: '',
-            });
-    }
-})
